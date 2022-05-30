3d03fc1a2b930d0874fd85dc61e9bbb5be694247
Jan Kratochvil <kratochvil.jan@outlook.com>
2022-05-30 17:40:21 +0200

    Adds redirects to support prior Worpress paths

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/:slug([\\w\\-]+)',
+        destination: '/blog/:slug',
+        permanent: true
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
