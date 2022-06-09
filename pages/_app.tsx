e5282408cc109e136580aa980f49dcf11b79d310
Jan Kratochvil <kratochvil.jan@outlook.com>
2022-06-09 18:46:47 +0200

    Adds basic analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { init, trackPages } from "insights-js"
+import { INSIGHTS_ID } from '../const/insights'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  init(INSIGHTS_ID)
+  trackPages()
+
+  return <Component {...pageProps} />
 }
 
 export default MyApp;
