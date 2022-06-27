24436426d26e6b0e352a45c289c73f37266336de
Jan Kratochvil <kratochvil.jan@outlook.com>
2022-06-27 14:42:52 +0200

    Removes Twitter description

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -58,7 +58,6 @@ const Post: NextPage<BlogProps> = ({
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@jankratochvilcz" />
         <meta name="twitter:title" content={post.title} />
-        <meta name="twitter:description" content={post.preview} />
         {twitterImagePath && (
           <>
             <meta property="og:image" content={twitterImagePath} />
