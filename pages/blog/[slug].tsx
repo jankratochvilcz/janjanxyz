156f8186acbd3e49d612a55b216ad44878b3c902
Jan Kratochvil <kratochvil.jan@outlook.com>
2022-06-27 14:55:43 +0200

    Revert "Tries to use a working OG image from the Internet"
    This reverts commit 45887eb3d4b68ddee454479b8a5944bdbfb200d9.

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -54,7 +54,6 @@ const Post: NextPage<BlogProps> = ({
         <title>{post.title} | Jan Kratochvil</title>
         <meta property="og:title" content={post.title} />
         <meta property="og:description" content={post.preview} />
-        <meta property="og:type" content="website" />
         <meta property="og:url" content={canonicalUrl} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@jankratochvilcz" />
@@ -63,14 +62,13 @@ const Post: NextPage<BlogProps> = ({
         {twitterImagePath && (
           <>
             <meta
+              name="og:image"
               property="og:image"
               content={twitterImagePath}
             />
-            <meta
-              property="twitter:image"
-              content={twitterImagePath}
-            />
             <meta name="og:image:alt" content={post.title} />
+            <meta name="og:image:width" content="1200" />
+            <meta name="og:image:height" content="628" />
           </>
         )}
       </Head>
